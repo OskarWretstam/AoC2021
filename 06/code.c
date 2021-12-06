457ca12278a0b0e0be50c597d2805ef4c199b257
Wretstam Oskar <oskar.wretstam.ow@gmail.com>
2021-12-06 20:19:02 +0100

    Day 6 part 2

--- 06/code.c
+++ 06/code.c
@@ -2,36 +2,30 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define MAX_NOF 10000000
+#define MAX_NOF 1000000
+#define AGE_CLASSES 9
 
 
 int main(int argc, char* argv[])
 {
    int c;
-   char *a = realloc(NULL, MAX_NOF*sizeof(char));
-   if(a == NULL)
-   {
-      printf("Error 1\n");
-      return 1;
-   }
-
-   long long unsigned nof = 0;
+   char a[MAX_NOF], b[MAX_NOF];
+   long long unsigned nofa = 0, nofb = 0;
 
    while ((c = getchar()) != EOF)
    {
       if(c != ',' && c != '\n')
       {
-         a[nof++] = c-48;
+         a[nofa++] = c-48;
+         b[nofb++] = c-48;
       }
    }
 
-
    int to_add;
-   int allocs = 1;
-   for(int i = 0; i < 256; i++)
+   for(int i = 0; i < 80; i++)
    {
       to_add = 0;
-      for(int j = 0; j < nof; j++)
+      for(int j = 0; j < nofa; j++)
       {
          if(a[j] != 0)
          {
@@ -46,22 +40,46 @@ int main(int argc, char* argv[])
 
       for(int j = 0; j < to_add; j++)
       {
-         if(nof == MAX_NOF)
+         if(nofa == MAX_NOF)
          {
-            printf("Realloc\n");
-            allocs++;
-            char *a = realloc(a, allocs*MAX_NOF*sizeof(char));
-
-            if(a == NULL)
-            {
-               printf("Error 2\n");
-               return 1;
-            }
+            printf("Error\n");
+            return 1;
          }
 
-         a[nof++] = 8;
+         a[nofa++] = 8;
       }
    }
 
-   printf("%llu\n", nof);
+   printf("%lld\n", nofa);
+
+   long long int x[AGE_CLASSES];
+   long long int to_add_long;
+   memset(x, 0, AGE_CLASSES*sizeof(long long int));
+
+   for(int i = 0; i < nofb; i++)
+   {
+      x[b[i]]++;
+   }
+
+   for(int i = 0; i < 256; i++)
+   {
+      to_add_long = x[0];
+
+      for(int j = 0; j < AGE_CLASSES-1; j++)
+      {
+         x[j] = x[j+1];
+      }
+
+      x[6] += to_add_long;
+      x[8] = to_add_long;
+   }
+
+   nofb = 0;
+   for(int i = 0; i < AGE_CLASSES; i++)
+   {
+      nofb += x[i];
+   }
+
+   printf("%lld\n", nofb);
+
 }
